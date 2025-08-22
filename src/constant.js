df30b6270e42cd34c9a472f0b95dbf81a07e0a0f
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-08-22 08:34:58 +0500

    gemini url

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,1 @@
-export const url =
-  'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCZ75JxWRqRkVLheov0Zr2lbFEYRiRnYdQ';
+export const url = import.meta.env.VITE_API_URL;
